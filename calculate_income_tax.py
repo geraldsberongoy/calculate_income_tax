6b0eda15cf67d852aa030e504fb358ad85548206
geraldsberongoy <geraldsberongoy@iskolarngbayan.pup.edu.ph>
2024-01-23 23:07:36 +0800

    Code

--- calculate_income_tax.py
+++ calculate_income_tax.py
@@ -10,3 +10,21 @@
 # For example, suppose the taxable income is 45000, and the income tax payable is
 
 # 10000*0% + 10000*10%  + 25000*20% = $6000.
+
+# CODE
+
+income = 45_000
+
+if income <= 10_000:
+    total_tax = 0 * 10_000
+elif income <= 20_000:
+    total_tax = (income - 10_000) * 0.10
+else:
+    tax_below_10k = 0 * 10_000
+    tax_below_20k = (20_000 - 10_000) * 0.10
+    tax_above_20k = (income - 20000) * 0.20
+    total_tax = tax_below_10k + tax_below_20k + tax_above_20k
+
+print("Total tax to pay is", total_tax)
+
+    
